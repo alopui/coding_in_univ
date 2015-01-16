d4cb4bb98c3dbf00741b5b30fb1eae0cf5049a65
Sungchul Choi <sc82.choi@gachon.ac.kr>
2015-01-17 01:55:19 +0900

    Add Funcgtion, Closer, Variable, Logic Examples

---
+++ JavaScript/Function.js
@@ -0,0 +1,17 @@
+/**
+ * Created by blissray on 15. 1. 17.
+ */
+
+
+function helloGachon(){
+    console.log("Hello Gachon");
+}
+
+function callYourName(name){
+    return "Hello" + name;
+}
+
+helloGachon()
+
+var hG = helloGachon();
+hG
